d0aff4eec4a5411c6815ed4e3cd7c290c4299f06
AloisSeckar <ellrohir@seznam.cz>
2025-08-29 21:14:16 +0200

    test: suite of test file samples

---
+++ test/src/test-config-default.ts
@@ -0,0 +1,15 @@
+export default {
+  cosca: {
+    // this comment must be preserved
+    stringKey: 'value',
+    numberKey: 1,
+    boolKey: true,
+    arrayKey: [
+      'value',
+    ],
+    objectKey: {
+      // this comment must be preserved
+      nestedKey: 'value',
+    },
+  },
+}
---
+++ test/src/test-config-named.ts
@@ -0,0 +1,15 @@
+export const config = {
+  cosca: {
+    // this comment must be preserved
+    stringKey: 'value',
+    numberKey: 1,
+    boolKey: true,
+    arrayKey: [
+      'value',
+    ],
+    objectKey: {
+      // this comment must be preserved
+      nestedKey: 'value',
+    },
+  },
+}
