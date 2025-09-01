c0291845895f3d973f62b0ceaa43f6a5152066e9
Alois Sečkár <ellrohir@seznam.cz>
2025-09-01 19:17:17 +0200

    test: new suite for createFileFromTemplate

--- test/create-file-from-template.test.ts
+++ test/create-file-from-template.test.ts
@@ -1,6 +1,38 @@
-import { expect, test } from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 import { createFileFromTemplate } from '../src/main'
+import { getConsoleSpy, readNormalizedFile } from './cosca-test-utils'
 
-test('should be defined', () => {
-  expect(createFileFromTemplate).toBeDefined()
+describe('Test createFileFromTemplate function', () => {
+
+  let wd: string = ''
+  let spy: any
+
+  beforeEach(() => {
+    wd = process.env.WORKSPACE_DIR!
+    spy = getConsoleSpy('log')
+  })
+
+  test('should be defined', () => {
+    expect(createFileFromTemplate).toBeDefined()
+  })
+
+  test('should create the file from local source', async () => {
+    // path must be - package name:relative/path/to/file
+    await createFileFromTemplate(`elrh-cosca:test/fixtures/text-file.txt`, `${wd}/local-file-copy.txt`, true)
+
+    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/successfully created/))
+
+    await expect(readNormalizedFile(wd, 'local-file-copy.txt')).toMatchFileSnapshot('snapshots/created-local-file.txt')
+  })
+
+  // vitest must be installed (but this is a requirement for running vitest tests)
+  test('should create the file from NPM source', async () => {
+    // path must be - package name:relative/path/to/file
+    await createFileFromTemplate(`vitest:README.md`, `${wd}/npm-file-copy.txt`, true)
+
+    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/successfully created/))
+
+    await expect(readNormalizedFile(wd, 'npm-file-copy.txt')).toMatchFileSnapshot('snapshots/created-npm-file.txt')
+  })
+
 })
