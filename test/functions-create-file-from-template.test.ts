a5468dde223f38e277036b5c54f9f21c6b552023
AloisSeckar <ellrohir@seznam.cz>
2025-09-04 16:47:28 +0200

    test: new cases to cover failures

--- test/functions-create-file-from-template.test.ts
+++ test/functions-create-file-from-template.test.ts
@@ -1,6 +1,7 @@
+import { existsSync } from 'node:fs'
 import { beforeEach, describe, expect, test } from 'vitest'
 import { createFileFromTemplate } from '../src/main'
-import { getConsoleSpy, readNormalizedFile } from './cosca-test-utils'
+import { getConsoleSpy, getPromptSpy, readNormalizedFile } from './cosca-test-utils'
 
 describe('Test createFileFromTemplate function', () => {
 
@@ -14,6 +15,18 @@ describe('Test createFileFromTemplate function', () => {
 
   test('should be defined', () => {
     expect(createFileFromTemplate).toBeDefined()
+  })
+  
+  test('should fail because invalid path', async () => {
+    await expect(createFileFromTemplate(`path`, `${wd}/local-file-copy.txt`, true)).rejects.toThrow(/Invalid input/)
+    
+    expect(existsSync(`${wd}/local-file-copy.txt`)).toBe(false)
+  })
+
+  test('should fail because uknown path', async () => {
+    await expect(createFileFromTemplate(`elrh-cosca:path`, `${wd}/local-file-copy.txt`, true)).rejects.toThrow(/Template file not found at/)
+    
+    expect(existsSync(`${wd}/local-file-copy.txt`)).toBe(false)
   })
 
   test('should create the file from local source', async () => {
@@ -34,5 +47,7 @@ describe('Test createFileFromTemplate function', () => {
 
     await expect(readNormalizedFile(wd, 'npm-file-copy.txt')).toMatchFileSnapshot('snapshots/created-npm-file.txt')
   })
+
+  // TODO test with user input simulation
 
 })
